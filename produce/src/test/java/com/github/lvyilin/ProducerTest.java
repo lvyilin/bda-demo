1ba0fe0f5b8bc8820b172b0657a1ded401611e40
lvyilin <lvyilin48@foxmail.com>
2019-06-04 23:46:12 +0800

    Add: small data (200 records) Update: producer

--- produce/src/test/java/com/github/lvyilin/ProducerTest.java
+++ produce/src/test/java/com/github/lvyilin/ProducerTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple Producer.
  */
-public class AppTest 
+public class ProducerTest
 {
     /**
      * Rigorous Test :-)
